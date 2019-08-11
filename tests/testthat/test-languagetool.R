d148151c584dc48d6a4a5de8662a930fb7a41e4a
Vilmantas Gegzna <GegznaV@gmail.com>
2019-08-11 18:03:24 +0300

    Unit test updated
    Test

--- tests/testthat/test-languagetool.R
+++ tests/testthat/test-languagetool.R
@@ -1,6 +1,20 @@
-test_that("languagetool works with `tagger_only = TRUE`", {
+test_that("languagetool works with text, file and directory input`", {
   
-  expect_silent({output <- languagetool(test_text, tagger_only = TRUE)})
-  expect_is(output, "character")
+  file_name <- system.file(package = "LanguageToolR", "test", "test_text.txt")
+  dir_name  <- system.file(package = "LanguageToolR", "test")
   
+  # tagger_only = TRUE is used to get the output quicer
+  expect_silent({output_text <- languagetool(test_text, tagger_only = TRUE)})
+  expect_silent({output_file <- languagetool(file_name, tagger_only = TRUE)})
+  expect_silent({output_dir  <- languagetool(dir_name,  tagger_only = TRUE)})
+  
+  expect_is(output,      "character")
+  expect_is(output_file, "character")
+  expect_is(output_dir,  "character")
+  
+  # TODO: More tests are needed here to investigate the contents of the output.
+  # E.g., to check the equivalence of the output with the same text but from 
+  # different sources (text, file, directory).
+
 })
+
