534d4fbdaea5e194199e1aab662321906eec7b6d
Clemens Schmid <clemens@nevrome.de>
2020-07-27 22:28:45 +0200

    check for identical output with vector, text file and directory input

--- tests/testthat/test-languagetool.R
+++ tests/testthat/test-languagetool.R
@@ -3,18 +3,19 @@ test_that("languagetool works with text, file and directory input`", {
   file_name <- system.file(package = "LanguageToolR", "test", "test_text.txt")
   dir_name  <- system.file(package = "LanguageToolR", "test")
   
-  # tagger_only = TRUE is used to get the output quicer
+  # tagger_only = TRUE is used to get the output quicker
   expect_silent({output_text <- languagetool(test_text, tagger_only = TRUE)})
-  expect_silent({output_file <- languagetool(file_name, tagger_only = TRUE)})
-  expect_silent({output_dir  <- languagetool(dir_name,  tagger_only = TRUE)})
+  expect_silent({output_file <- languagetool(input_file = file_name, tagger_only = TRUE)})
+  expect_silent({output_dir  <- languagetool(input_directory = dir_name,  tagger_only = TRUE, recursive = TRUE)})
   
   expect_is(output_text, "character")
   expect_is(output_file, "character")
   expect_is(output_dir,  "character")
   
-  # TODO: More tests are needed here to investigate the contents of the output.
-  # E.g., to check the equivalence of the output with the same text but from 
-  # different sources (text, file, directory).
+  # check the equivalence of the output with the same text but from different sources (text, file, directory)
+  expect_true(
+    all(sapply(list(output_text, output_dir), FUN = identical, output_file))
+  )
   
 })
 
